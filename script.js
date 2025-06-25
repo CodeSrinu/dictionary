5c51a589a65089bc35fd3a814cf4ba4707f7b480
Srinu <srinu18yt@gmail.com>
2025-06-25 22:05:11 +0530

    added pb to aiResultdiv

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
-const API_KEY = "AIzaSyChbTSxUwc_95mRTEefPsrCDhm8lukU4vk";
+const API_KEY = process.env.GOOGLE_AI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
